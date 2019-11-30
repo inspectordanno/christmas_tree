d93c29f48cb3d24bb44564259a1d14b9048b4b7f
Dan Spector <danno2423@gmail.com>
2019-11-30 14:33:54 -0500

    updating generateTrees()

--- index.js
+++ index.js
@@ -47,28 +47,33 @@ const sketch = (p) => {
 
     //width of the row is equal to the window width minus the margin
     //there is also a margin on the left side which increases depending on the row number
-    let rowWidth = windowWidth - xStart - xEnd; 
-    console.log(rowWidth)
+    let rowWidth = windowWidth - xStart - xEnd;
+    const pyramidHeight = windowHeight - margin.top - margin.bottom;
 
     const trees = []; //array holding tree objects
 
-    for (let i = 0; i < numberOfTrees; i++) {
-      trees.push(new Tree(i, xStart, yPos)); //push tree to trees array
-    
-        //each tree in the current row has a width and a gap between them
-        //if the total of their width and gap is less than or equal to the rowWidth, proceed in the current row
-        if (treesInCurrentRow * (treeWidth + xGap) <= rowWidth) {
-          treesInCurrentRow += 1; //add tree to current row
-          xStart = xStart + treeWidth + xGap; //adjust x position - add treeWidth and xGap
-        } else if (treesInCurrentRow * (xGap + treeWidth) > rowWidth) {
-          console.log(treesInCurrentRow);
-          treesInCurrentRow = 0;
-          rowNumber += 1;
-          xStart = margin.left + (rowNumber * (xGap + treeWidth)); //reset x position to the left margin to the rowNumber times the xGap
-          xEnd = margin.right + (rowNumber * (xGap + treeWidth));
-          yPos = yPos - (treeHeight + yGap); //move the y position up by the height and gap
-          rowWidth = windowWidth - xStart - xEnd;
+    const generateTrees = () => {
+      for (let i = 0; i < numberOfTrees; i++) {
+          trees.push(new Tree(i, xStart, yPos)); //push tree to trees array
+          //each tree in the current row has a width and a gap between them
+          //if the total of their width and gap is less than or equal to the rowWidth, proceed in the current row
+          if (treesInCurrentRow * (treeWidth + xGap) <= rowWidth) {
+            treesInCurrentRow += 1; //add tree to current row
+            xStart = xStart + treeWidth + xGap; //adjust x position - add treeWidth and xGap
+          } else if (treesInCurrentRow * (xGap + treeWidth) > rowWidth) {
+            treesInCurrentRow = 0;
+            rowNumber += 1;
+            xStart = margin.left + (rowNumber * (xGap + treeWidth)); //reset x position to the left margin to the rowNumber times the xGap
+            xEnd = margin.right + (rowNumber * (xGap + treeWidth));
+            yPos = yPos - (treeHeight + yGap); //move the y position up by the height and gap
+            rowWidth = windowWidth - xStart - xEnd;
+         } else if (rowNumber * (treeHeight + yGap) > pyramidHeight) {
+           return;
+         }
+      };
     };
+
+    generateTrees();
 
     p.preload = () => {
       img = p.loadImage(tree);
@@ -86,6 +91,5 @@ const sketch = (p) => {
       })
     };
   };
-};
 
 new p5(sketch, containerElement);
